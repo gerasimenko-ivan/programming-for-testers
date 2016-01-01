07fe759c60e769ac40a111209fca4725f3c2bee0
Ivan Gerasimenko <gistmp@gmail.com>
2016-01-02 01:07:52 +0300

    add method List<GroupData> getGroups()

--- addressbook-selenium-tests/src/com/example/tests/GroupCreationTests.java
+++ addressbook-selenium-tests/src/com/example/tests/GroupCreationTests.java
@@ -1,5 +1,8 @@
 package com.example.tests;
 
+import java.util.List;
+import static org.testng.Assert.assertEquals;
+
 import org.testng.annotations.Test;
 
 public class GroupCreationTests extends TestBase {
@@ -7,6 +10,11 @@ public class GroupCreationTests extends TestBase {
   public void testNonEmptyGroupCreation() throws Exception {
 	app.getNavigationHelper().openMainPage();
     app.getNavigationHelper().gotoGroupsPage();
+    
+    // save old state
+    List<GroupData> initialGroups = app.getGroupHelper().getGroups();
+    
+    // actions
     app.getGroupHelper().initGroupCreation();
     GroupData group = new GroupData();
     group.name = "test-group-4";
@@ -15,9 +23,15 @@ public class GroupCreationTests extends TestBase {
 	app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupForm();
     app.getGroupHelper().returnToGroupsPage();
+    
+    // get new state
+    List<GroupData> newGroups = app.getGroupHelper().getGroups();
+    
+    // compare states
+    assertEquals(newGroups.size(), initialGroups.size() + 1);
   }
   
-  @Test
+  //@Test
   public void testEmptyGroupCreation() throws Exception {
 	app.getNavigationHelper().openMainPage();
     app.getNavigationHelper().gotoGroupsPage();
