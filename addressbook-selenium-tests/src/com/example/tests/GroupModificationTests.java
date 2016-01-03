81cddd87265ef7ada4afc559cf80ecc5569db0db
Ivan Gerasimenko <gistmp@gmail.com>
2016-01-03 14:43:23 +0300

    add assert to renameGroup()

--- addressbook-selenium-tests/src/com/example/tests/GroupModificationTests.java
+++ addressbook-selenium-tests/src/com/example/tests/GroupModificationTests.java
@@ -1,19 +1,37 @@
 package com.example.tests;
 
+import static org.testng.Assert.assertEquals;
+
+import java.util.Collections;
+import java.util.List;
+
 import org.testng.annotations.Test;
 
 public class GroupModificationTests extends TestBase {
 	
 	@Test
-	public void removeGroup() {
+	public void renameGroup() {
 		app.getNavigationHelper().openMainPage();
 	    app.getNavigationHelper().gotoGroupsPage();
 	    
-	    app.getGroupHelper().initGroupModification(1);
+	    // save initial groups
+	    List<GroupData> initialGroups = app.getGroupHelper().getGroups();
+	    
+	    // actions
 	    GroupData group = new GroupData();
 	    group.name = "new name";
-		app.getGroupHelper().fillGroupForm(group );
+	    app.getGroupHelper().initGroupModification(0);
+		app.getGroupHelper().fillGroupForm(group);
 	    app.getGroupHelper().submitGroupModification();
 	    app.getGroupHelper().returnToGroupsPage();
+	    
+	    // save new groups
+	    List<GroupData> newGroups = app.getGroupHelper().getGroups();
+	    
+	    // compare states
+	    initialGroups.remove(0);
+	    initialGroups.add(group);
+	    Collections.sort(initialGroups);
+	    assertEquals(newGroups, initialGroups);
 	}
 }
