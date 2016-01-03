8ec7c6f1382815291fc5b3089b19ec1f1a2f0a88
Ivan Gerasimenko <gistmp@gmail.com>
2016-01-03 16:32:29 +0300

    format source

--- addressbook-selenium-tests/src/com/example/tests/GroupCreationTests.java
+++ addressbook-selenium-tests/src/com/example/tests/GroupCreationTests.java
@@ -7,54 +7,55 @@ import static org.testng.Assert.assertEquals;
 import org.testng.annotations.Test;
 
 public class GroupCreationTests extends TestBase {
-  @Test
-  public void testNonEmptyGroupCreation() throws Exception {
-	app.getNavigationHelper().openMainPage();
-    app.getNavigationHelper().gotoGroupsPage();
-    
-    // save old state
-    List<GroupData> initialGroups = app.getGroupHelper().getGroups();
-    
-    // actions
-    GroupData group = new GroupData();
-    group.name = "test-group-4";
-    group.header = "HEADER\ntest-group-4";
-    group.footer = "FOOTER\ntest-group-4";
-    app.getGroupHelper().initGroupCreation();
-	app.getGroupHelper().fillGroupForm(group);
-    app.getGroupHelper().submitGroupForm();
-    app.getGroupHelper().returnToGroupsPage();
-    
-    // get new state
-    List<GroupData> newGroups = app.getGroupHelper().getGroups();
-    
-    // compare states
-    initialGroups.add(group);
-    Collections.sort(initialGroups);
-    assertEquals(newGroups, initialGroups);
-  }
-  
-  @Test
-  public void testEmptyGroupCreation() throws Exception {
-	app.getNavigationHelper().openMainPage();
-    app.getNavigationHelper().gotoGroupsPage();
-    
-    // save initial groups
-    List<GroupData> initialGroups = app.getGroupHelper().getGroups();
-    
-    // actions
-    GroupData group = new GroupData("", "", "");
-    app.getGroupHelper().initGroupCreation();
-	app.getGroupHelper().fillGroupForm(group);
-    app.getGroupHelper().submitGroupForm();
-    app.getGroupHelper().returnToGroupsPage();
-    
-    // save new groups
-    List<GroupData> newGroups = app.getGroupHelper().getGroups();
-    
-    // compare states
-    initialGroups.add(group);
-    Collections.sort(initialGroups);
-    assertEquals(newGroups, initialGroups);
-  }
+	
+	@Test
+	public void testNonEmptyGroupCreation() throws Exception {
+		app.getNavigationHelper().openMainPage();
+		app.getNavigationHelper().gotoGroupsPage();
+
+		// save old state
+		List<GroupData> initialGroups = app.getGroupHelper().getGroups();
+
+		// actions
+		GroupData group = new GroupData();
+		group.name = "test-group-4";
+		group.header = "HEADER\ntest-group-4";
+		group.footer = "FOOTER\ntest-group-4";
+		app.getGroupHelper().initGroupCreation();
+		app.getGroupHelper().fillGroupForm(group);
+		app.getGroupHelper().submitGroupForm();
+		app.getGroupHelper().returnToGroupsPage();
+
+		// get new state
+		List<GroupData> newGroups = app.getGroupHelper().getGroups();
+
+		// compare states
+		initialGroups.add(group);
+		Collections.sort(initialGroups);
+		assertEquals(newGroups, initialGroups);
+	}
+
+	@Test
+	public void testEmptyGroupCreation() throws Exception {
+		app.getNavigationHelper().openMainPage();
+		app.getNavigationHelper().gotoGroupsPage();
+
+		// save initial groups
+		List<GroupData> initialGroups = app.getGroupHelper().getGroups();
+
+		// actions
+		GroupData group = new GroupData("", "", "");
+		app.getGroupHelper().initGroupCreation();
+		app.getGroupHelper().fillGroupForm(group);
+		app.getGroupHelper().submitGroupForm();
+		app.getGroupHelper().returnToGroupsPage();
+
+		// save new groups
+		List<GroupData> newGroups = app.getGroupHelper().getGroups();
+
+		// compare states
+		initialGroups.add(group);
+		Collections.sort(initialGroups);
+		assertEquals(newGroups, initialGroups);
+	}
 }
