7624dd409d7fa6c32d26e8ea23c25492c26f47c4
Ivan Gerasimenko <gistmp@gmail.com>
2016-01-05 18:07:30 +0300

    add assert to ContactRemovalTests

--- addressbook-selenium-tests/src/com/example/tests/ContactRemovalTests.java
+++ addressbook-selenium-tests/src/com/example/tests/ContactRemovalTests.java
@@ -1,5 +1,9 @@
 package com.example.tests;
 
+import static org.testng.Assert.assertEquals;
+
+import java.util.List;
+
 import org.testng.annotations.Test;
 
 public class ContactRemovalTests extends TestBase {
@@ -7,8 +11,20 @@ public class ContactRemovalTests extends TestBase {
 	@Test
 	public void removeContact() {
 		app.getNavigationHelper().openMainPage();
-		app.getContactHelper().initContactEdit(1);
+
+		// save initial contacts list
+		List<ContactData> initialContacts = app.getContactHelper().getContacts();
+
+		// actions
+		app.getContactHelper().initContactEdit(0);
 		app.getContactHelper().submitDeleteContact();
 		app.getContactHelper().returnToHomePage();
+
+		// save new contact list
+		List<ContactData> newContacts = app.getContactHelper().getContacts();
+
+		// compare states
+		initialContacts.remove(0);
+		assertEquals(newContacts, initialContacts);
 	}
 }
