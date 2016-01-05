cb4e62c24ecd084a2704ae8fb4e71fb8a88789aa
Ivan Gerasimenko <gistmp@gmail.com>
2016-01-05 21:22:50 +0300

    add assert to ContactModificationTests

--- addressbook-selenium-tests/src/com/example/tests/ContactModificationTests.java
+++ addressbook-selenium-tests/src/com/example/tests/ContactModificationTests.java
@@ -1,5 +1,10 @@
 package com.example.tests;
 
+import static org.testng.Assert.assertEquals;
+
+import java.util.Collections;
+import java.util.List;
+
 import org.testng.annotations.Test;
 
 public class ContactModificationTests extends TestBase {
@@ -7,12 +12,29 @@ public class ContactModificationTests extends TestBase {
 	@Test
 	public void modifyContact() {
 		app.getNavigationHelper().openMainPage();
-		app.getContactHelper().initContactEdit(1);
+
+		// save initial contacts list
+		List<ContactData> initialContacts = app.getContactHelper().getContacts();
+
+		// actions
+		app.getContactHelper().initContactEdit(0);
 		ContactData contact = new ContactData();
 		contact.firstName = "New first name";
 		contact.lastName = "New last name";
+		contact.email_1 = "new@new.nw";
 		app.getContactHelper().fillContactForm(contact);
 		app.getContactHelper().submitUpdate();
 		app.getContactHelper().returnToHomePage();
+
+		// save new contact list
+		List<ContactData> newContacts = app.getContactHelper().getContacts();
+
+		// compare states
+		ContactData oldContact = initialContacts.get(0);
+		initialContacts.remove(0);
+		oldContact.update(contact);
+		initialContacts.add(0, oldContact);
+		Collections.sort(initialContacts);
+		assertEquals(newContacts, initialContacts);
 	}
 }
