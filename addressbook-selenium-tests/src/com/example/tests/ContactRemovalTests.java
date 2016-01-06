6d95e8ba7ec319d4fce73adf4d6d7161e27ff1ad
Ivan Gerasimenko <gistmp@gmail.com>
2016-01-07 02:20:50 +0300

    delete many contacts

--- addressbook-selenium-tests/src/com/example/tests/ContactRemovalTests.java
+++ addressbook-selenium-tests/src/com/example/tests/ContactRemovalTests.java
@@ -16,19 +16,21 @@ public class ContactRemovalTests extends TestBase {
 		// save initial contacts list
 		List<ContactData> initialContacts = app.getContactHelper().getContacts();
 		
-		Random rnd = new Random();
-		int index = rnd.nextInt(initialContacts.size() - 1);
+		for (int i = 0; i < 25 && initialContacts.size() > 20; i++) {
+			Random rnd = new Random();
+			int index = rnd.nextInt(initialContacts.size() - 1);
 
-		// actions
-		app.getContactHelper().initContactEdit(index);
-		app.getContactHelper().submitDeleteContact();
-		app.getContactHelper().returnToHomePage();
+			// actions
+			app.getContactHelper().initContactEdit(index);
+			app.getContactHelper().submitDeleteContact();
+			app.getContactHelper().returnToHomePage();
+			initialContacts.remove(index);
+		}
 
 		// save new contact list
 		List<ContactData> newContacts = app.getContactHelper().getContacts();
 
 		// compare states
-		initialContacts.remove(index);
 		assertEquals(newContacts, initialContacts);
 	}
 }
