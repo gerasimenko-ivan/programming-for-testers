ff6348647da49d62a0197baaa4a52f6d749a6bd3
Ivan Gerasimenko <gistmp@gmail.com>
2016-01-07 01:27:24 +0300

    parametrization of modifyContact(ContactData contact)

--- addressbook-selenium-tests/src/com/example/tests/ContactModificationTests.java
+++ addressbook-selenium-tests/src/com/example/tests/ContactModificationTests.java
@@ -4,24 +4,25 @@ import static org.testng.Assert.assertEquals;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 import org.testng.annotations.Test;
 
 public class ContactModificationTests extends TestBase {
 
-	@Test
-	public void modifyContact() {
+	@Test(dataProvider = "randomValidContactGenerator")
+	public void modifyContact(ContactData contact) {
 		app.getNavigationHelper().openMainPage();
 
 		// save initial contacts list
 		List<ContactData> initialContacts = app.getContactHelper().getContacts();
 
+		Random rnd = new Random();
+		int index = rnd.nextInt(initialContacts.size() - 1);
+
 		// actions
-		app.getContactHelper().initContactEdit(0);
-		ContactData contact = new ContactData();
-		contact.firstName = "New first name";
-		contact.lastName = "New last name";
-		contact.email_1 = "new@new.nw";
+		app.getContactHelper().initContactEdit(index);
+		contact.contactGroup = null;
 		app.getContactHelper().fillContactForm(contact);
 		app.getContactHelper().submitUpdate();
 		app.getContactHelper().returnToHomePage();
@@ -29,9 +30,15 @@ public class ContactModificationTests extends TestBase {
 		// save new contact list
 		List<ContactData> newContacts = app.getContactHelper().getContacts();
 
+		// move single email_2 to first position
+		if (contact.email_1 == "") {
+			contact.email_1 = contact.email_2;
+			contact.email_2 = "";
+		}
+
 		// compare states
-		ContactData oldContact = initialContacts.get(0);
-		initialContacts.remove(0);
+		ContactData oldContact = initialContacts.get(index);
+		initialContacts.remove(index);
 		oldContact.update(contact);
 		initialContacts.add(oldContact);
 		Collections.sort(initialContacts);
