d960098d4a4bd553396e537ac81f8f17a8da9332
Ivan Gerasimenko <gistmp@gmail.com>
2016-01-06 15:54:58 +0300

    test parametrization. add DataProvider

--- addressbook-selenium-tests/src/com/example/tests/GroupCreationTests.java
+++ addressbook-selenium-tests/src/com/example/tests/GroupCreationTests.java
@@ -1,15 +1,42 @@
 package com.example.tests;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Random;
+
 import static org.testng.Assert.assertEquals;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class GroupCreationTests extends TestBase {
-	
-	@Test
-	public void testNonEmptyGroupCreation() throws Exception {
+
+	@DataProvider
+	public Iterator<Object[]> randomValidGroupGenerator() {
+		List<Object[]> list = new ArrayList<Object[]>();
+		for (int i = 0; i < 5; i++) {
+			GroupData group = new GroupData();
+			group.name = generateRandomString();
+			group.header = generateRandomString();
+			group.footer = generateRandomString();
+			list.add(new Object[] { group });
+		}
+		return list.iterator();
+	}
+
+	public String generateRandomString() {
+		Random rnd = new Random();
+		if (rnd.nextInt(3) == 0) {
+			return "";
+		} else {
+			return "test " + rnd.nextInt();
+		}
+	}
+
+	@Test(dataProvider = "randomValidGroupGenerator")
+	public void testValidGroupCreation(GroupData group) throws Exception {
 		app.getNavigationHelper().openMainPage();
 		app.getNavigationHelper().gotoGroupsPage();
 
@@ -17,40 +44,12 @@ public class GroupCreationTests extends TestBase {
 		List<GroupData> initialGroups = app.getGroupHelper().getGroups();
 
 		// actions
-		GroupData group = new GroupData();
-		group.name = "test-group-4";
-		group.header = "HEADER\ntest-group-4";
-		group.footer = "FOOTER\ntest-group-4";
 		app.getGroupHelper().initGroupCreation();
 		app.getGroupHelper().fillGroupForm(group);
 		app.getGroupHelper().submitGroupForm();
 		app.getGroupHelper().returnToGroupsPage();
 
 		// get new state
-		List<GroupData> newGroups = app.getGroupHelper().getGroups();
-
-		// compare states
-		initialGroups.add(group);
-		Collections.sort(initialGroups);
-		assertEquals(newGroups, initialGroups);
-	}
-
-	@Test
-	public void testEmptyGroupCreation() throws Exception {
-		app.getNavigationHelper().openMainPage();
-		app.getNavigationHelper().gotoGroupsPage();
-
-		// save initial groups
-		List<GroupData> initialGroups = app.getGroupHelper().getGroups();
-
-		// actions
-		GroupData group = new GroupData("", "", "");
-		app.getGroupHelper().initGroupCreation();
-		app.getGroupHelper().fillGroupForm(group);
-		app.getGroupHelper().submitGroupForm();
-		app.getGroupHelper().returnToGroupsPage();
-
-		// save new groups
 		List<GroupData> newGroups = app.getGroupHelper().getGroups();
 
 		// compare states
