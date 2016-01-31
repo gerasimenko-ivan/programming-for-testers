138afe8ddb217df0efa91106944e6b8467828434
Ivan Gerasimenko <gistmp@gmail.com>
2016-01-31 17:33:42 +0300

    add 'xmlpull-1.1.3.1.jar'

--- addressbook-selenium-tests/run-testng.bat
+++ addressbook-selenium-tests/run-testng.bat
@@ -1,1 +1,1 @@
-java -cp bin;C:\Tools\Xml\xstream-1.4.8\lib\xstream-1.4.8.jar;C:\Tools\Xml\xstream-1.4.8\lib\xstream\xpp3_min-1.1.4c.jar;C:\Tools\FunctionalTesting\selenium-server-standalone-2.48.2.jar org.testng.TestNG %1
+java -cp bin;C:\Tools\Xml\xstream-1.4.8\lib\xstream-1.4.8.jar;C:\Tools\Xml\xstream-1.4.8\lib\xstream\xpp3_min-1.1.4c.jar;C:\Tools\Xml\xstream-1.4.8\lib\xstream\xmlpull-1.1.3.1.jar;C:\Tools\FunctionalTesting\selenium-server-standalone-2.48.2.jar org.testng.TestNG %1
